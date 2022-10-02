50af961b01b2f464f01222e6381802030bdb9437
francovici <88690552+VictoriaFigini@users.noreply.github.com>
2022-10-01 21:38:39 -0300

    forum

---
+++ src/components/__VLS_template.js
@@ -0,0 +1,14 @@
+import sourceData from '@/data.json';
+
+export default (await import('vue')).defineComponent({
+data() {
+return [
+threads, sourceData.threads
+];
+}
+});
+function __VLS_template() {
+// @ts-ignore
+[threads, thread];
+return {};
+}
